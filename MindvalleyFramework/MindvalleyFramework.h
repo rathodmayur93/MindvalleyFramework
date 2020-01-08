6ddc8942c8d8523e03fc16347264a6a9493c83dd
Mayur Rathod <rathodmayur93@gmail.com>
2020-01-08 17:31:33 +0300

    Initial Commit

---
+++ MindvalleyFramework/MindvalleyFramework.h
@@ -0,0 +1,19 @@
+//
+//  MindvalleyFramework.h
+//  MindvalleyFramework
+//
+//  Created by ds-mayur on 1/8/20.
+//  Copyright © 2020 Mayur Rathod. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MindvalleyFramework.
+FOUNDATION_EXPORT double MindvalleyFrameworkVersionNumber;
+
+//! Project version string for MindvalleyFramework.
+FOUNDATION_EXPORT const unsigned char MindvalleyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MindvalleyFramework/PublicHeader.h>
+
+
